2ecdbaa3edde7924077e982d81da83bd90f0e4dc
justspace <js4std@gmail.com>
2023-10-09 02:55:30 +0500

    some work

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import FilmCard from './components/film-card';
+import PageContent from './components/page-content';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -7,6 +10,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <FilmCard />
+    <PageContent />
   </React.StrictMode>
 );
