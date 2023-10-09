f95d13a8f632594cc3a7dceee4e06132a6d7cad5
JustASpace <js4std@gmail.com>
2023-10-10 00:52:29 +0500

    task 2 done

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/app';
-
+import {App} from './components/app/app';
+import {Settings} from './mocks/settings';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App name={Settings.name} genre={Settings.genre} trailerDateRelease={Settings.trailerDateRelease}/>
   </React.StrictMode>
 );
