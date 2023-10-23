9db87998e4b3b1ea79086827481b0f89a21136f8
JustASpace <js4std@gmail.com>
2023-10-23 06:27:21 +0500

    module4

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,12 @@
 import {MainPage} from '../../pages/main-page/main-page';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {SingInPage} from '../../pages/sing-in-page/sing-in-page';
+import {NotFoundPage} from '../../pages/not-found-page/not-found-page';
+import {MyListPage} from '../../pages/my-list-page/my-list-page';
+import {MoviePage} from '../../pages/movie-page/movie-page';
+import {MovieReviewsPage} from '../../pages/movie-reviews-page/movie-reviews-page';
+import {PlayerPage} from '../../pages/player-page/player-page';
+import {PrivateRoute} from '../private-route/private-route';
 
 export type AppProps = {
   name: string;
@@ -9,7 +17,22 @@ export type AppProps = {
 
 export function App({name, genre, trailerDateRelease}: AppProps) {
   return (
-    <div>
-      <MainPage name={name} genre={genre} trailerDateRelease={trailerDateRelease}/>
-    </div>);
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<MainPage name={name} genre={genre} trailerDateRelease={trailerDateRelease}/>} />
+          <Route path='login' element={<SingInPage />} />
+          <Route path='mylist' element={
+            <PrivateRoute >
+              <MyListPage />
+            </PrivateRoute >
+          }
+          />
+          <Route path='films/:id' element={<MoviePage />} />
+          <Route path='films/:id/review' element={<MovieReviewsPage />} />
+          <Route path='player/:id' element={<PlayerPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />}/>
+      </Routes>
+    </BrowserRouter>);
 }
